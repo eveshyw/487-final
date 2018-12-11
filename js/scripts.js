20442e49d335a96f800730b38bb5f9adddd93d30
Yuwei Qian <eveshyw@gmail.com>
2018-12-11 17:23:19 -0500

    Looped through the JSON data

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   console.log('Ready to begin the test...');
 
+  // Toggle with button
     $( ".mobile-btn" ).click(function() {
     $( "nav" ).toggle();
   });
 
+  // Brenda function
   var student = {
     name: 'Brenda',
     age: 20,
@@ -14,4 +16,30 @@ $(document).ready(function() {
   if (student.likeCoding){
     console.log(student.name+", "+student.age+" years old, loves coding.");
   }
+
+
+  // Call JSON data
+  $.ajax({
+    type:"GET",
+    url:
+    "./js/refugees.json",
+    dataType: "json",
+    async:true,
+    success:function(data){
+      console.log(data);
+      buildContent(data);
+    },
+    error:function(error){
+      console.log(error);
+    }
+  })
 });
+
+
+function buildContent(data){
+  for (var i=0; i<data.length; i++){
+    console.log("Origin: "+data[i].Origin);
+    console.log("Value: "+data[i].Value);
+    $(".cards").append("<div class='card'><p class='country'>"+data[i].Origin+"</p><p class='value'>"+data[i].Value+"Requests</p></div>");
+  }
+}
